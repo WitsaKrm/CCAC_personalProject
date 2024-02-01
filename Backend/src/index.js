4778544e1c2e857edde5dbf0200dfee48c785c75
WitsaKrm <143206207+WitsaKrm@users.noreply.github.com>
2024-02-01 10:34:19 +0700

    create middleware

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,12 +4,21 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoute = require("./routes/auth-route");
+const notFound = require('./middlewares/notFound');
+const errorMiddleware = require('./middlewares/error');
 const env = process.env;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoute);
+
+
+//not found
+app.use(notFound)
+// error
+app.use(errorMiddleware)
+
 
 const port = env.PORT 
 app.listen(port, () => {
