2cfa4d1d8075f124f538e444748ea30c35d23647
WitsaKrm <143206207+WitsaKrm@users.noreply.github.com>
2024-02-18 22:57:38 +0700

    use sx sm md lg xl 2xl

--- Frontend/src/layouts/guest-page.jsx
+++ Frontend/src/layouts/guest-page.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import endpoint from "../services/axios";
 import Header from "../components/header-component";
+// import "./Guest.css"; // Import your CSS file
 
 export default function Guest() {
   const [rooms, setRooms] = useState([]);
@@ -27,26 +28,30 @@ export default function Guest() {
           { label: "login", url: "/login" },
         ]}
       />
-      <div className="bg-gray-100 min-h-screen">
+      <div className="bg-gray-100 min-h-screen mx-auto">
         <div className="container mx-auto p-4">
           <h1 className="text-2xl text-gray-500 font-bold mb-8">
             {`rooms`.toUpperCase()}
           </h1>
 
-          <div className="flex w-100% flex-wrap gap-4 justify-center items-center mx-8 ">
+          <div className="grid sx:grid-cols-2  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-7 gap-4 justify-center items-center mx-8 max-w-full ">
             {rooms.map((room) => (
               <div
                 key={room.id}
-                className={`p-4 rounded-md shadow-md  ${
+                className={`p-4 rounded-md shadow-md ${
                   room.status === "available"
-                    ? "bg-white hover:text-green-400 cursor-pointer"
-                    : "bg-gray-200"
+                    ? "bg-white hover:text-green-400 cursor-pointer hover:bg-green-100"
+                    : "bg-gray-200 hover:bg-red-200" 
                 }`}
               >
                 <h2 className="text-xl text-gray-500 font-semibold mb-4">
                   {room.room_number}
                 </h2>
-                <p>This is some content for room {room.room_number}.</p>
+                <p className='text-sm text-gray-700 truncate '>
+                  This is some content for room {room.room_number}. Lorem ipsum
+                  dolor sit amet. Lorem ipsum dolor sit amet, consectetur
+                  adipisicing elit. Quam, culpa?
+                </p>
               </div>
             ))}
           </div>
