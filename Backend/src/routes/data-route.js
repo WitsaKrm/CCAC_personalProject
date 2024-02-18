ecf435ff050b205a0313999963276be39681030c
WitsaKrm <143206207+WitsaKrm@users.noreply.github.com>
2024-02-18 18:30:05 +0700

    data route update

--- Backend/src/routes/data-route.js
+++ Backend/src/routes/data-route.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const roomController = require("../controllers/room-controller");
+const userController = require("../controllers/user-controller");
+const billController = require("../controllers/bill-controller");
 
 ///// rooms /////
 router.get("/rooms", roomController.getAllRooms);
-router.post("/rooms", roomController.getAllRooms);
-router.put("/rooms/:id", roomController.getAllRooms);
-router.delete("/rooms/:id", roomController.getAllRooms);
+router.post("/rooms", roomController.createRoom);
+router.put("/rooms/:id", roomController.updateRoom);
+router.delete("/rooms/:id", roomController.deleteRoom);
+
+///// users /////
+router.get("/users", userController.getAllUsers);
+router.post("/users", userController.createUser);
+router.put("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
+
+///// bills /////
+router.get("/bills", billController.getAllBills);
+router.post("/bills", billController.createBill);
+router.put("/bills/:id", billController.updateBill);
+router.delete("/bills/:id", billController.deleteBill);
+
 module.exports = router;
