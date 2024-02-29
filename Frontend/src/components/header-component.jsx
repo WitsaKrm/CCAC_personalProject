c1ba8be514e3bc661204681cbbbc0550d07e03e0
WitsaKrm <143206207+WitsaKrm@users.noreply.github.com>
2024-02-29 07:18:34 +0700

    Header

--- Frontend/src/components/header-component.jsx
+++ Frontend/src/components/header-component.jsx
@@ -18,11 +18,11 @@ export default function Header({ links, page }) {
   ));
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 ">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">XXXX Dormitory</a>
       </div>
-      <div className="flex-none">
+      <div className="flex-none sx:hidden">
         {page === "guest" || page === "register" ? (
           <ul className="menu menu-horizontal px-1 mx-2">{navigationItems}</ul>
         ) : page === "login" ? (
